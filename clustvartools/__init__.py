7199d2be8262a41b7da9927afb3fe9e30340a2e6
duverier <duverierdjifack@gmail.com>
2026-09-20 16:01:11 +0200

    update _init_ file

--- clustvartools/__init__.py
+++ clustvartools/__init__.py
@@ -4,3 +4,8 @@ from __future__ import annotations
 from .clustering import *
 from .graphics import *
 from .others import *
+
+__version__ = '0.0.1.post1'
+__name__ = "clustvartools"
+__author__ = 'Duverier DJIFACK ZEBAZE'
+__email__ = 'djifacklab@gmail.com'
